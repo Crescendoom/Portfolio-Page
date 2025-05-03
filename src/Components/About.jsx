0ecce2b5defc13cbf7326666fc866d4dc93d380f
Laurence Hernandez <133084008+Crescendoom@users.noreply.github.com>
2025-05-03 09:48:25 +0000

    Refactor code structure for improved readability and maintainability

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Computer Science student studying at Adamson University, specializing in Data Science. I am passionate about transforming raw data into actionable insights through the use of statistical analysis, machine learning, and data visualization.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Placeholder",
+  "Placeholder",
+  "Placeholder",
+  "Placeholder",
+  "Placeholder",
+  "Placeholder",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I aim to apply data science techniques to solve real-world problems in areas such as business, healthcare, and social impact. I’m constantly seeking opportunities to learn, collaborate, and contribute to meaningful data-driven projects.";
 
 const About = () => {
   return (
